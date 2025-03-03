fabebeeb686467ff66bf4c6c6eac444a8e796f66
Radexman <Borderlandsmaniak@gmail.com>
2025-03-03 17:35:28 +0100

    Experience setup

---
+++ src/Experience/Experience.js
@@ -0,0 +1,9 @@
+import Sizes from './Utils/Sizes';
+
+export default class Experience {
+	constructor(canvas) {
+		window.experience = this;
+		this.canvas = canvas;
+		this.sizes = new Sizes();
+	}
+}
