3860444cbb1806a20d6cf304707b1ac63288bfd9
Radexman <Borderlandsmaniak@gmail.com>
2025-03-06 15:47:56 +0100

    Debug ui

--- src/Experience/Utils/Debug.js
+++ src/Experience/Utils/Debug.js
@@ -1,5 +1,11 @@
+import GUI from 'lil-gui';
+
 export default class Debug {
 	constructor() {
-		console.log('Debug started');
+		this.active = window.location.hash === '#debug';
+
+		if (this.active) {
+			this.ui = new GUI();
+		}
 	}
 }
